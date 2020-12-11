2675fb8f222e70a46227d451727c1dad12eecec3
Tapu Mandal <online.tapu@gmail.com>
2020-12-11 18:34:38 +0600

    Working on pagination

--- src/app/_module/cart/cart-list/cart-list.component.ts
+++ src/app/_module/cart/cart-list/cart-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { data } from 'jquery';
 import { ErrorManagementService } from 'src/app/_services/error-management.service';
 import { NetworkcallingService } from 'src/app/_services/networkcalling.service';
+import { Cart } from 'src/app/_model/cart.model';
+import { Pagination } from 'src/app/_model/pagination.model';
+
 
 @Component({
   selector: 'app-cart-list',
@@ -10,16 +13,28 @@ import { NetworkcallingService } from 'src/app/_services/networkcalling.service'
 })
 export class CartListComponent implements OnInit {
 
+  carts: Cart[];
+  pagination: Pagination;
+  collection: number[];
+
   constructor(private networkCalling : NetworkcallingService, private errorManagement : ErrorManagementService) { }
 
   ngOnInit(): void {
     this.loadCartList();
+
+    // for(let i=1; i<=this.pagination.pageSize; i++){
+    //   let Obj = {'page': `${i}`,'size': `20`};
+    //   this.collection.push(Obj);
+    // }
   }
 
   loadCartList() {
     this.networkCalling.getCartList().subscribe(
       data => {
-        console.log(data.data);
+        this.carts = data.data;
+        this.pagination = data.myPagenation;
+        console.log(this.pagination);
+        this.collection = Array(this.pagination.totalElement).fill(this.pagination.totalElement).map((x,i)=>i+1);
       },
       err => {
         this.errorManagement.responseFaield(err);
--- src/app/_module/cart/cart.module.ts
+++ src/app/_module/cart/cart.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartUpdateComponent } from './cart-update/cart-update.component';
+import { NgxPaginationModule } from "ngx-pagination";
 
 
 @NgModule({
   declarations: [CartListComponent, CartUpdateComponent],
   imports: [
     CommonModule,
-    CartRoutingModule
+    CartRoutingModule,
+    NgxPaginationModule
   ]
 })
 export class CartModule { }
