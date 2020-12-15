accf4336a14dd17e2f73c6c61f07eaf95fb35204
Tapu Mandal <online.tapu@gmail.com>
2020-12-16 02:01:08 +0600

    Working on Print

--- src/app/_module/cart/cart-update/cart-update.component.ts
+++ src/app/_module/cart/cart-update/cart-update.component.ts
@@ -37,6 +37,52 @@ export class CartUpdateComponent implements OnInit {
     this.hideInPrint = true;
   }
 
+  printCustomerCopy(){
+    this.isCustomer = true;
+    this.hideInPrint = true;
+  }
+  printCompanyCopy(){
+    this.isCustomer = false;
+    this.hideInPrint = true;
+
+    // const printContent = document.getElementById("invoice");
+    // const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+    // WindowPrt.document.write(printContent.innerHTML);
+    // WindowPrt.document.close();
+    // WindowPrt.focus();
+    // WindowPrt.print();
+    // WindowPrt.close();
+
+    
+      var prtContent = document.getElementById("invoice");
+      var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+      
+      WinPrint.document.write('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
+      
+      // To keep styling
+      var file = WinPrint.document.createElement("link");
+      file.setAttribute("rel", "stylesheet");
+      file.setAttribute("type", "text/css");
+      file.setAttribute("href", 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+      WinPrint.document.head.appendChild(file);
+  
+      
+      // WinPrint.document.write(prtContent.innerHTML);
+      // WinPrint.document.close();
+      // WinPrint.setTimeout(function(){
+      //   WinPrint.focus();
+      //   WinPrint.print();
+      //   WinPrint.close();
+      // }, 1000);
+
+  }
+  invoiceReset(){
+    this.isCustomer = false;
+    this.hideInPrint = false;
+  }
+
+
+
   update(){
     console.log("Update");
   }
